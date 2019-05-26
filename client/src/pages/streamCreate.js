c1758b43e5b77200982aff80ec466500c06aff88
Kumar Abhirup <kumar@iqubex.com>
2019-05-26 13:44:32 +0530

    👌 IMPROVE: Integrate redux-form

--- client/src/pages/streamCreate.js
+++ client/src/pages/streamCreate.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react'
+import { Field, reduxForm } from 'redux-form'
 
-export default class streamCreate extends Component {
-  render() {
+import Button from '../components/Button'
+
+class streamCreate extends Component {
+  renderInput = ({ input, label }) => {
     return (
-      <div>
-        Create Stream
+      <div className="field">
+        <label>{ label }</label>
+        <input type="text" {...input} />
       </div>
     )
   }
+
+  onSubmit = payload => {
+    console.log(payload)
+  }
+ 
+  render() {
+    return (
+      <>
+      <h1>Create a Stream</h1>
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field name="description" component={this.renderInput} label="Enter Description" />
+        <Button color="primary">Stream!</Button>
+      </form>
+      </>
+    )
+  }
 }
+
+export default reduxForm({
+  form: 'streamCreate'
+})(streamCreate)
