6d1e98ca0a5024d8a49eb2b5c76572e83ef7af2b
Kumar Abhirup <kumar@iqubex.com>
2019-05-26 11:44:27 +0530

    👌 IMPROVE: Support Redux Devtools

--- client/src/components/ProvideRedux.js
+++ client/src/components/ProvideRedux.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 
 import reducers from '../redux/reducers'
 
 export default class ProvideRedux extends Component {
+  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+  
+  store = createStore(reducers, this.composeEnhancers(applyMiddleware()))
+
   render() {
     return (
-      <Provider store={createStore(reducers)}>
+      <Provider store={this.store} >
         { this.props.children }
       </Provider>
     )
