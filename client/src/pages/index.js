14d311724957f8f3c08a2866f652cccd1f3893d0
Kumar Abhirup <kumar@iqubex.com>
2019-05-27 11:01:36 +0530

    👌 IMPROVE: Show Create a Stream button

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import { fetchStreams } from '../redux/actions/streamActions'
-import Button from '../components/Button';
+import Button from '../components/Button'
 
 class indexPage extends Component {
   componentDidMount() {
@@ -26,7 +27,7 @@ class indexPage extends Component {
           ) }
 
           <i className="large middle aligned icon camera"></i>
-          
+
           <div className="content">
             <h3>{ stream.title }</h3>
             <div className="description">
@@ -45,6 +46,12 @@ class indexPage extends Component {
         <center><h1>Streams</h1></center>
         <div className="ui celled list" style={{width: '1000px', margin: '30px auto'}}>
           { this.renderList() }
+          <br />
+          { this.props.isSignedIn && (
+            <div style={{width: '100%', textAlign: 'right'}}>
+              <Link to="/streams/new"><Button icon="play" color="green">Create a Stream</Button></Link>
+            </div>
+          ) }
         </div>
       </>
     )
@@ -54,7 +61,8 @@ class indexPage extends Component {
 const mapStateToProps = state => {
   return {
     streams: Object.values(state.streams), // Convert object `{1: {}, 2: {}}` into an array like `[{}, {}]` so...
-    currentUserId: state.auth.userId
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn
   }
 }
 
