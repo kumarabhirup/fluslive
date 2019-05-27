56879687889ace29d8f498fa5cd5deb5d8d5cfdb
Kumar Abhirup <kumar@iqubex.com>
2019-05-27 10:29:00 +0530

    👌 IMPROVE: Show stream list

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class indexPage extends Component {
+import { fetchStreams } from '../redux/actions/streamActions'
+
+class indexPage extends Component {
+  componentDidMount() {
+    this.props.fetchStreams()
+  }
+
+  renderList() {
+    return this.props.streams.map(stream => {
+      return (
+        <div className="item" key={stream.id} style={{padding: '10px 0px'}}>
+          <i className="large middle aligned icon camera"></i>
+          <div className="content">
+            <h3>{ stream.title }</h3>
+            <div className="description">
+              { stream.description }
+            </div>
+          </div>
+        </div>
+      )
+    })
+  }
+
   render() {
     return (
-      <div>
-        Stream List
-      </div>
+      <>
+        <center><h1>Streams</h1></center>
+        <div className="ui celled list" style={{width: '1000px', margin: '30px auto'}}>
+          { this.renderList() }
+        </div>
+      </>
     )
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    streams: Object.values(state.streams) // Convert object `{1: {}, 2: {}}` into an array like `[{}, {}]` so...
+  }
+}
+
+export default connect(mapStateToProps, {
+  fetchStreams
+})(indexPage)
