3349036f99f9801ec4ac3d32a6f3ffe579aa5673
Kumar Abhirup <kumar@iqubex.com>
2019-05-28 13:58:05 +0530

    👌 IMPROVE: Add a video player

--- client/src/pages/streamShow.js
+++ client/src/pages/streamShow.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
+import flv from 'flv.js'
 import { connect } from 'react-redux'
 
 import { Spinner } from '../components/Loading'
 import { fetchStream } from '../redux/actions/streamActions'
 
 class streamShow extends Component {
+  constructor(props) {
+    super(props)
+    this.videoRef = createRef()
+  }
+
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id)
   }
@@ -13,18 +19,21 @@ class streamShow extends Component {
     const title = this.props.stream && this.props.stream.title
     const description = this.props.stream && this.props.stream.description
     return (
-      <>
+      <div style={{padding: '10px 0px'}}>
       {
         this.props.stream ?
         (
           <>
-            <h1>{ title }</h1>
-            <p>{ description }</p>
+            <video ref={this.videoRef} style={{width: '80%', margin: '0px auto', display: 'block'}} controls />
+            <div style={{width: '80%', margin: '30px auto', display: 'block'}}>
+              <h1>{ title }</h1>
+              <p>{ description }</p>
+            </div>
           </>
         )
         : <Spinner />
       }
-      </>
+      </div>
     )
   }
 }
