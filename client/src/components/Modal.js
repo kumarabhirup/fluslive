e97829eeee017a090505a97cdf0f6ae7ccba8722
Kumar Abhirup <kumar@iqubex.com>
2019-05-28 10:19:53 +0530

    📦 NEW: Create reusable modal component

---
+++ client/src/components/Modal.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { createPortal } from 'react-dom'
+
+export default class Modal extends Component {
+  render() {
+    return createPortal(
+      (<div className="ui dimmer modals visible active" onClick={this.props.onBackgroundClick}>
+        <div className="ui standard modal visible active" onClick={e => e.stopPropagation()}>
+          {
+            !this.props.children ?
+            (
+            <>
+              <div className="header">{this.props.header}</div>
+              <div className="content">
+                {this.props.content}
+              </div>
+              <div className="actions">
+                {this.props.actions}
+              </div>
+            </>
+            )
+            : this.props.children
+          }
+        </div>
+      </div>),
+      document.querySelector(this.props.domQuery || '#modal')
+    )
+  }
+}
--- client/src/pages/streamDelete.js
+++ client/src/pages/streamDelete.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
+import Modal from '../components/Modal'
+
+import Button from '../components/Button'
+import history from '../lib/historyObject'
 
 export default class streamDelete extends Component {
+  actions = (
+    <>
+      <Button icon="cancel">Cancel</Button>
+      <Button icon="trash" color="negative">Delete</Button>
+    </>
+  )
+
   render() {
     return (
       <div>
-        Delete Stream
+        Stream Delete
+        <Modal 
+          header="Delete Stream" 
+          content="Are you sure you wanna delete this stream?" 
+          actions={this.actions}
+          onBackgroundClick={() => history.push('/')}
+        />
       </div>
     )
   }
